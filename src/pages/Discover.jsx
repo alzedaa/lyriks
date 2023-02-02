f12b8dccebd006126f8d4fe69f02c66f7a3cf180
Ayman Alzedawy <ayman.alzedawy@gmail.com>
2023-02-01 21:11:31 -0500

    Added react-icons and fixed discover page to display songs fetched from api

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,8 +1,11 @@
 import { Error, Loader, SongCard } from "../components";
 import { genres } from "../assets/constants";
 import { useGetTopChartsQuery } from "../redux/services/shazamCore";
+import { useDispatch, useSelector } from "react-redux";
 
 const Discover = () => {
+  const dispatch = useDispatch();
+  const { activeSong, isPlaying } = useSelector((state) => state.player);
   const { data, isFetching, error } = useGetTopChartsQuery();
   const genreTitle = "Pop";
   console.log("25");
@@ -29,7 +32,14 @@ const Discover = () => {
       </div>
       <div className="flex flex-wrap sm:justify-start justify-center gap-8 text-white">
         {data?.map((song, i) => (
-          <SongCard key={song.key} song={song} i={i} />
+          <SongCard
+            key={song.key}
+            song={song}
+            isPlaying={isPlaying}
+            activeSong={activeSong}
+            data={data}
+            i={i}
+          />
         ))}
       </div>
     </div>
