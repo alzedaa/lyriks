5ca72b4f71b8b07888e4a9b0e4a9fe0f8c792f80
Ayman Alzedawy <ayman.alzedawy@gmail.com>
2023-02-04 18:00:19 -0500

    Addition of artist details and song collection of artist. Around you page beginning

--- src/pages/AroundYou.jsx
+++ src/pages/AroundYou.jsx
@@ -1,5 +1,41 @@
-import React from 'react';
+import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { useSelector } from "react-redux";
+import { useGetSongsByCountryQuery } from "../redux/services/shazamCore";
 
-const CountryTracks = () => <div>CountryTracks</div>;
+import { Error, Loader, SongCard } from "../components";
 
-export default CountryTracks;
+const AroundYou = () => {
+  const [country, setCountry] = useState("");
+  const [loading, setLoading] = useState(true);
+  const { activeSong, setActiveSong } = useSelector((state) => state.player);
+  const { data, isFetching, error } = useGetSongsByCountryQuery(country);
+
+  console.log(country);
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://geo.ipify.org/api/v2/country?apiKey=at_0GZxGxZ9PUA2hZ2Z2srZxRInaSsVM"
+      )
+      .then((res) => setCountry(res?.data?.location?.country))
+      .catch((err) => console.log(err))
+      .finally(() => setLoading(false));
+  }, [country]);
+  if (isFetching && loading) return <Loader title="Loading songs around you" />;
+
+  if (error && country) return <Error />;
+
+  return (
+    <div className="flex flex-col">
+      <h2 className="font-bold text-3xl text-white text-left mt-4 mb-10">
+        Around You:
+      </h2>
+
+      <div className="flex flex-wrap sm:justify-start justify-center gap-8"></div>
+    </div>
+  );
+};
+
+export default AroundYou;
--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -16,7 +16,7 @@ fetch("https://shazam-core.p.rapidapi.com/v1/charts/world", options)
 export const shazamCoreApi = createApi({
   reducerPath: "shazamCoreApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://shazam-core.p.rapidapi.com/v1",
+    baseUrl: "https://shazam-core.p.rapidapi.com/",
     prepareHeaders: (headers) => {
       headers.set("X-RapidAPI-Key", "");
 
@@ -24,11 +24,26 @@ export const shazamCoreApi = createApi({
     },
   }),
   endpoints: (builder) => ({
-    getTopCharts: builder.query({ query: () => "/charts/world" }),
+    getTopCharts: builder.query({ query: () => "v1/charts/world" }),
     getSongDetails: builder.query({
-      query: ({ songid }) => `/tracks/details?track_id=${songid}`,
+      query: ({ songid }) => `v1/tracks/details?track_id=${songid}`,
+    }),
+    getSongRelated: builder.query({
+      query: ({ songid }) => `v1/tracks/related?track_id=${songid}`,
+    }),
+    getArtistDetails: builder.query({
+      query: (artistId) => `v2/artists/details?artist_id=${artistId}`,
+    }),
+    getSongsByCountry: builder.query({
+      query: (countryCode) => `v1//charts/country?country_code=${countryCode}`,
     }),
   }),
 });
 
-export const { useGetTopChartsQuery, useGetSongDetailsQuery } = shazamCoreApi;
+export const {
+  useGetTopChartsQuery,
+  useGetSongDetailsQuery,
+  useGetSongRelatedQuery,
+  useGetArtistDetailsQuery,
+  useGetSongsByCountryQuery,
+} = shazamCoreApi;
