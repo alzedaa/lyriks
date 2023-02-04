66b21f36478d323756cb636ab58e5259c04070ff
Ayman Alzedawy <ayman.alzedawy@gmail.com>
2023-02-04 11:59:09 -0500

    Addition of song lyrics and details when clicking song

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -18,17 +18,17 @@ export const shazamCoreApi = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: "https://shazam-core.p.rapidapi.com/v1",
     prepareHeaders: (headers) => {
-      headers.set(
-        "X-RapidAPI-Key",
-        ""
-      );
+      headers.set("X-RapidAPI-Key", "");
 
       return headers;
     },
   }),
   endpoints: (builder) => ({
     getTopCharts: builder.query({ query: () => "/charts/world" }),
+    getSongDetails: builder.query({
+      query: ({ songid }) => `/tracks/details?track_id=${songid}`,
+    }),
   }),
 });
 
-export const { useGetTopChartsQuery } = shazamCoreApi;
+export const { useGetTopChartsQuery, useGetSongDetailsQuery } = shazamCoreApi;
