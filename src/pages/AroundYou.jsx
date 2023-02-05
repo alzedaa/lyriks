58232f9a8aa69a936ac3e775ded151fb0d6e9a3c
Ayman Alzedawy <ayman.alzedawy@gmail.com>
2023-02-05 14:30:26 -0500

    Completed most of requirements for app to function. Some errors exist to be fixed at later date

--- src/pages/AroundYou.jsx
+++ src/pages/AroundYou.jsx
@@ -9,7 +9,7 @@ import { Error, Loader, SongCard } from "../components";
 const AroundYou = () => {
   const [country, setCountry] = useState("");
   const [loading, setLoading] = useState(true);
-  const { activeSong, setActiveSong } = useSelector((state) => state.player);
+  const { activeSong, isPlaying } = useSelector((state) => state.player);
   const { data, isFetching, error } = useGetSongsByCountryQuery(country);
 
   console.log(country);
@@ -31,9 +31,21 @@ const AroundYou = () => {
     <div className="flex flex-col">
       <h2 className="font-bold text-3xl text-white text-left mt-4 mb-10">
         Around You:
+        <span className="font-black"> {country}</span>
       </h2>
 
-      <div className="flex flex-wrap sm:justify-start justify-center gap-8"></div>
+      <div className="flex flex-wrap sm:justify-start justify-center gap-8">
+        {data?.map((song, i) => (
+          <SongCard
+            key={song.key}
+            song={song}
+            isPlaying={isPlaying}
+            activeSong={activeSong}
+            data={data}
+            i={i}
+          />
+        ))}
+      </div>
     </div>
   );
 };
